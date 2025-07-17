56e75c0b5f48ea6288642838b098f832dd6231e8
phaothu2002x <truongthuan21342@gmail.com>
2025-07-17 15:58:57 +0700

    movie detail page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,18 @@
+import { icons } from '@/constants/icons';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 const Profile = () => {
     return (
-        <View>
-            <Text>Profile</Text>
+        <View className="bg-primary flex-1 px-10">
+            <View className="flex justify-center items-center gap-5 flex-1 flex-col">
+                <Image
+                    source={icons.person}
+                    tintColor="#fff"
+                    className="size-10"
+                />
+                <Text className="text-gray-500 text-base">Profile</Text>
+            </View>
         </View>
     );
 };
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,18 @@
+import { icons } from '@/constants/icons';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 const Saved = () => {
     return (
-        <View>
-            <Text>Saved</Text>
+        <View className="bg-primary flex-1 px-10">
+            <View className="flex justify-center items-center gap-5 flex-1 flex-col">
+                <Image
+                    source={icons.save}
+                    tintColor="#fff"
+                    className="size-10"
+                />
+                <Text className="text-gray-500 text-base">SAVE</Text>
+            </View>
         </View>
     );
 };
